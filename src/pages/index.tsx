e0454f4a65e4d787c7aa2364ec9e7209411f4e38
kthatoto <kthatoto@gmail.com>
2023-03-25 03:18:19 +0900

    Add style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
+import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
+
+import styles from "@/styles/Home.module.css";
+
 const Home = () => {
-  return <></>;
+  return (
+    <div className={styles.wrapper}>
+    </div>
+  );
 };
+
 export default Home;
