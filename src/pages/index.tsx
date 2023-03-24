2c1a93c67936ed242faf0413f9add50705255f54
kthatoto <kthatoto@gmail.com>
2023-03-25 04:34:35 +0900

    Add draggable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,15 @@ import styles from "@/styles/Home.module.css";
 const Home = () => {
   return (
     <div className={styles.wrapper}>
+      <Draggable>
+        <div
+          className={styles.draggable}
+          style={{
+            width: 100,
+            height: 100,
+          }}
+        ></div>
+      </Draggable>
     </div>
   );
 };
