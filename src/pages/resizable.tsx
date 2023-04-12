9f85550cfb4fe8c9885d7f08de4bd41bd048fa5b
kthatoto <kthatoto@gmail.com>
2023-04-12 15:57:13 +0900

    Complete resizable

--- src/pages/resizable.tsx
+++ src/pages/resizable.tsx
@@ -3,30 +3,41 @@ import { useCallback, useRef, useState } from "react";
 
 const MIN_WIDTH = 50;
 const MIN_HEIGHT = 50;
+
 export default () => {
   const [size, setSize] = useState({ width: 100, height: 100 });
+  const [resizingSize, setResizingSize] = useState(size);
 
+  const draggableNodeRef = useRef(null);
   const resizeNodeRef = useRef(null);
 
   const onStartResize = useCallback((e: DraggableEvent) => {
     e.stopPropagation();
   }, []);
+  const onStopResize = useCallback(() => {
+    setSize(resizingSize);
+  }, [resizingSize]);
 
   const onResize = useCallback((_e: DraggableEvent, data: DraggableData) => {
-    setSize({
-      width: Math.max(MIN_WIDTH, size.width + data.deltaX),
-      height: Math.max(MIN_HEIGHT, size.height + data.deltaY),
+    setResizingSize({
+      width: Math.max(MIN_WIDTH, size.width + data.x),
+      height: Math.max(MIN_HEIGHT, size.height + data.y),
     });
   }, [size]);
 
   return (
     <div style={{ width: "100%", height: "100vh" }}>
-      <Draggable>
-        <div style={{ background: "gray", position: "relative", ...size }}>
+      <Draggable nodeRef={draggableNodeRef}>
+        <div
+          ref={draggableNodeRef}
+          style={{ background: "gray", position: "relative", ...resizingSize }}
+        >
           <Draggable
             nodeRef={resizeNodeRef}
             onDrag={onResize}
             onStart={onStartResize}
+            onStop={onStopResize}
+            position={{ x: 0, y: 0 }}
             axis="none"
           >
             <div
