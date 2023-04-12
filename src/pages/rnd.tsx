41fa778b99b082ca0ae185da1798e2259b5c38d2
kthatoto <kthatoto@gmail.com>
2023-04-13 05:34:19 +0900

    Add rnd page

---
+++ src/pages/rnd.tsx
@@ -0,0 +1,17 @@
+import { Rnd } from "react-rnd";
+
+export default () => {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <Rnd
+        default={{ x: 0, y: 0, width: 100, height: 100 }}
+        minWidth={50}
+        maxHeight={50}
+        style={{ backgroundColor: "gray" }}
+        resizeHandleStyles={{
+          bottomRight: { backgroundColor: "blue" }
+        }}
+      />
+    </div>
+  );
+};
